83c7b5d544352114819faff79ffe43450b958059
stacey <av.kuzina@gmail.com>
2021-01-13 15:26:03 +0000

    updated api handlers
    implemented api methods
    added exception handling to api

--- ring-api/src/ring_api/core.clj
+++ ring-api/src/ring_api/core.clj
@@ -3,33 +3,82 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer :all]
-            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.absolute-redirects :refer :all]
+            [ring.middleware.json :refer [wrap-json-response, wrap-json-body wrap-json-params]]
             [ring.middleware.cors :refer [wrap-cors]]
-            [ring.util.response :refer [response]]
+            [ring.util.response :refer :all]
             [ring-api.db :as db]))
 
-(defn create-new-appointment [body] (response body))
+; Middleware
+(defn wrap-library-exception "library exception handling middleware"
+  [handler]
+  (fn [request]
+    (try (handler request)
+         (catch clojure.lang.ExceptionInfo e
+           (prn e)
+           (bad-request (.getMessage e))))))
 
-(defn get-appointment [id] (response id ))
-(defn update-appointment [id body] (response  body ))
-(defn delete-appointment [id] (response (str "I delete an appointment " id) ))
-(defn get-all-appointments  [] (response db/select-all-appointments))
+(defn wrap-fallback-exception "fallback exception handling middleware"
+  [handler]
+  (fn [request]
+    (try (handler request)
+         (catch Exception e
+           (prn e)
+           (status 500 "Internal Server Error")))))
 
+; Handlers
+(defn create-new-appointment
+  [body]
+  (created (db/create-appointment body)))
+
+(defn get-appointment
+  [id]
+  ((def search-result (db/search-appointment id))
+   (prn search-result)
+   (if (empty? search-result)
+     (not-found "Appointment Not Found")
+     (response search-result)))
+  )
+
+(defn update-appointment
+  [id body]
+  (def updated-result (db/update-appointment id body))
+  (if (> updated-result 0)
+    ; updated existing resource
+    (status 204)
+    ; create new resource
+    (created (db/create-appointment body)))
+  )
+
+(defn delete-appointment
+  [id]
+  (response (db/delete-appointment id)))
+
+(defn get-all-appointments
+  []
+  (response db/select-all-appointments))
+
+; Routes
 (defroutes app-routes-handler
            (context "/appointments" [] (defroutes appointments-routes
-                                       (GET "/" [] (get-all-appointments)))
+                                                  (GET "/" [] (get-all-appointments)))
                                        (POST "/" {body :body} (create-new-appointment body))
                                        (context "/:id" [id] (defroutes appointment-routes
-                                                            (GET "/" [] (get-appointment id)))
+                                                                       (GET "/" [] (get-appointment id)))
                                                             (PUT "/" {body :body} (update-appointment id body))
                                                             (DELETE "/" [] (delete-appointment id))))
 
            (route/not-found "Not Found"))
 
+; API
 (def rest-api
   (-> app-routes-handler
       (wrap-defaults api-defaults)
+      wrap-json-body
       (wrap-cors :access-control-allow-methods #{:get :post :delete :options}
                  :access-control-allow-headers #{:accept :content-type}
                  :access-control-allow-origin [#"http://localhost:8080"])
-      wrap-json-response))
+      wrap-json-response
+      wrap-library-exception
+      wrap-fallback-exception
+      ))
--- ring-api/src/ring_api/db.clj
+++ ring-api/src/ring_api/db.clj
@@ -20,25 +20,66 @@
   )
 
 ;Scripts to create a db table and seed the test data
-;
+
 ;(def appointments-table-ddl
-;     (jdbc/create-table-ddl :appointments [[:id "varchar(120)"]
-;                                           [:description "varchar(120)"]
-;                                           [:location "varchar(32)"]
-;                                           [:subject "varchar(32)"]
-;                                           [:calendar "varchar(32)"]
-;                                           [:start "varchar(32)"]
-;                                           [:end "varchar(32)"]]))
+;  (jdbc/create-table-ddl :appointments [[:id :int "not null" "auto_increment"]
+;                                        [:description "varchar(120)"]
+;                                        [:location "varchar(32)"]
+;                                        [:subject "varchar(32)" "not null"]
+;                                        [:calendar "varchar(32)" "not null"]
+;                                        [:start "varchar(32)" "not null"]
+;                                        [:end "varchar(32)" "not null"]]))
 ;(def create-appointments-table (jdbc/db-do-commands db-spec [appointments-table-ddl
-;                                                              "CREATE INDEX appointment_ix ON appointments ( id );"]))
+;                                                             "CREATE INDEX appointment_ix ON appointments (id);"
+;                                                             ]))
 ;(def seed-data-ddl
-;  (jdbc/insert-multi! db-spec :appointments [{:id "id21",
-;                                        :description "",
-;                                        :location "",
-;                                        :subject "ring api demo",
-;                                        :calendar "Room 1",
-;                                        :start "2021-01-11 04:05",
-;                                        :end "2021-01-11 04:25"}]))
+;  (jdbc/insert-multi! db-spec :appointments [{:description "",
+;                                              :location    "",
+;                                              :subject     "ring api demo",
+;                                              :calendar    "Room 1",
+;                                              :start       "2021-01-11 04:05",
+;                                              :end         "2021-01-11 04:25"}]))
+
+
+;todo exception handling wrapper and workon error messages
+;todo change interface to take db, table ...
 
 (def select-all-appointments
   (jdbc/query db-spec ["SELECT * FROM appointments"]))
+
+(defn create-appointment "takes appointment json as a parameter" [body]
+  (def response
+    (try
+      (jdbc/insert! db-spec :appointments body)
+         (catch Exception e
+           (throw (ex-info (str "Invalid data " + (.getMessage e)) {})))))
+  ;parses response and extract id ({:id 4})
+  (str ((nth response 0) :id))
+  )
+
+(defn search-appointment "searches appointment by id" [id]
+  (def response
+    (try
+      (jdbc/query db-spec ["SELECT * FROM appointments WHERE id = ?" id])
+         (catch Exception e
+           (throw (ex-info (str "Invalid data " + (.getMessage e)) {})))))
+  (if (empty? response) {} (nth response 0))
+  )
+
+(defn delete-appointment "deletes appointment by id" [id]
+  (def response
+    (try
+      (jdbc/delete! db-spec :appointments ["id = ?" id])
+      (catch Exception e
+        (throw (ex-info (str "Invalid data " + (.getMessage e)) {})))))
+  (str)
+  )
+
+(defn update-appointment "updates appointment by id" [id body]
+  (def response
+    (try
+      (jdbc/update! db-spec :appointments body ["id = ?" id])
+         (catch Exception e
+           (throw (ex-info (str "Invalid data " + (.getMessage e)) {})))))
+  (nth response 0)
+  )
